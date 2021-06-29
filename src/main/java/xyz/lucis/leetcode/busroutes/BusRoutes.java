fccda0ffc4c796e3ce78ed0befcdb5700e0e5e2e
hanruobing 30008513 <hanruobing1@huawei.com>
2021-06-29 21:17:04 +0800

    add question url

--- src/main/java/xyz/lucis/leetcode/busroutes/BusRoutes.java
+++ src/main/java/xyz/lucis/leetcode/busroutes/BusRoutes.java
@@ -9,6 +9,19 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * https://leetcode-cn.com/problems/bus-routes/
+ * 给你一个数组 routes ，表示一系列公交线路，其中每个 routes[i] 表示一条公交线路，第 i 辆公交车将会在上面循环行驶。
+ * <p>
+ * 例如，路线 routes[0] = [1, 5, 7] 表示第 0 辆公交车会一直按序列 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... 这样的车站路线行驶。
+ * 现在从 source 车站出发（初始时不在公交车上），要前往 target 车站。 期间仅可乘坐公交车。
+ * <p>
+ * 求出 最少乘坐的公交车数量 。如果不可能到达终点车站，返回 -1 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/bus-routes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class BusRoutes {
 
     public static void main(String[] args) {
